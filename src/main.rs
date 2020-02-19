fed0a5d539790f5b1f9e79ed730b1a3043f751ba
István Bozsó <bozso.istvan@csfk.mta.hu>
2020-02-19 17:24:28 +0100

    experimenting with command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 use std::error::Error;
 use std::result::Result;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
 
 use hdf5file::{Hdf5File};
 
+#[derive(Debug, StructOpt)]
+struct Main {
+    file_path: PathBuf,
+
+    #[structopt(subcommand)]
+    op: Op,
+}
+
+impl Main {
+    fn main(&self) -> Result<(), Box<dyn Error>> {
+        let mut file = Hdf5File::open_file(&self.file_path)?;
+
+        match &self.op {
+            Op::Get { object_path } => {
+                let object = file.get_object(object_path)?;
+                println!("{:?}", object);
+            }
+            Op::Ls => {
+                for object_path in file.object_paths()? {
+                    println!("{:?}", object_path?.to_str());
+                }
+            }
+        }
+        return Ok(());
+    }
+}
+
+#[derive(Debug, StructOpt)]
+enum Op {
+    Get { object_path: PathBuf },
+    Ls,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let f = Hdf5File::open_file("/home/istvan/test.hdf5")?;
-    
-    println!("Hello, world!");
-    
-    Ok(())
+    return Main::from_args().main();
 }
